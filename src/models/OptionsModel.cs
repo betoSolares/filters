119977f210a8c7aba2d2c4b2e57c67461743939b
Roberto Solares <betosolaresgar@gmail.com>
2020-06-29 10:37:03 -0600

    Change the image of the kernel on the screen

--- src/models/OptionsModel.cs
+++ src/models/OptionsModel.cs
@@ -20,11 +20,20 @@ namespace Filters.Models
             set => this.RaiseAndSetIfChanged(ref isManual, value);
         }
 
+        // Path for the kernel image
+        private string kernelImg;
+        public string KernelImg
+        {
+            get => kernelImg;
+            set => this.RaiseAndSetIfChanged(ref kernelImg, value);
+        }
+
         /// <summary>Constructor</summary>
         public OptionsModel()
         {
             path = "No image. Please select one.";
             isManual = false;
+            kernelImg = "/assets/Blurred.png";
         }
     }
 }
