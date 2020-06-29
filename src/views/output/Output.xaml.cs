9f98832117923360779e8f685506531e7c02164f
Roberto Solares <betosolaresgar@gmail.com>
2020-06-29 14:50:42 -0600

    Add validation for the path

--- src/views/output/Output.xaml.cs
+++ src/views/output/Output.xaml.cs
@@ -1,5 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Filters.Models;
+using System.IO;
+using System;
 
 namespace Filters.Views.Output
 {
@@ -11,10 +15,55 @@ namespace Filters.Views.Output
             InitializeComponent();
         }
 
+        /// <summary>Applied the kernel to the image</summary>
+        /// <param name="sender">The object that raised the event</param>
+        /// <param name="e">The object that is being handled</param>
+        private void GenerateNewImage(object sender, RoutedEventArgs e)
+        {
+            if (HasValidOptions())
+                Console.WriteLine("GENERATE");
+            else
+                Console.WriteLine("ERROR");
+        }
+
+        /// <summary>Check if has valid options to applied the kernel</summary>
+        /// <returns>True if all the options are valid</returns>
+        private bool HasValidOptions()
+        {
+            return PathExists() && ValidImage() && ValidMatrix();
+        }
+
         /// <summary>Initialize all the components</summary>
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        /// <summary>Check that the path for the image exists</summary>
+        /// <returns>True if the path exists and is a file</returns>
+        private bool PathExists()
+        {
+            MainWindowModel context = DataContext as MainWindowModel;
+            string path = context.Options.Path;
+
+            if (!string.IsNullOrEmpty(path) || !string.IsNullOrWhiteSpace(path))
+            {
+                if (!Directory.Exists(path))
+                    if (File.Exists(path))
+                        return true;
+                return false;
+            }
+            return false;
+        }
+
+        private bool ValidImage()
+        {
+            return true;
+        }
+
+        private bool ValidMatrix()
+        {
+            return true;
         }
     }
 }
