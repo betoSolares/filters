1d93b92a239df04cf35b781928408066e2a59d01
Roberto Solares <betosolaresgar@gmail.com>
2020-06-30 11:22:21 -0600

    Add method to create the folder to save the results

--- src/lib/ImageProcessing.cs
+++ src/lib/ImageProcessing.cs
@@ -1,18 +1,40 @@
+using System.IO;
+
 namespace ImageProcessing
 {
     public class Processing
     {
         // Properties
-        private readonly string Path;
+        private readonly string OriginalImgPath;
         private readonly string Kernel;
+        private readonly string SavePath;
 
         /// <summary>Constructor</summary>
         /// <param name="path">The path of the original image</param>
         /// <param name="kernel">The kernel to apply to the image</param>
         public Processing(string path, string kernel)
         {
-            Path = path;
+            OriginalImgPath = path;
             Kernel = kernel;
+            SavePath = GetSaveDirectory();
+        }
+
+        /// <summary>Get the directory to save the results</summary>
+        /// <returns>The path to save the results</returns>
+        private string GetSaveDirectory()
+        {
+            string current = Directory.GetCurrentDirectory();
+
+            string save;
+            if (current.Contains(Path.Combine("filters", "src", "bin", "Debug", "netcoreapp3.0")))
+                save = Path.Combine(Directory.GetParent(current).Parent.Parent.FullName, "src/results");
+            else if (current.Contains(Path.Combine("filters", "src", "bin", "Release", "netcoreapp3.0")))
+                save = Path.Combine(Directory.GetParent(current).Parent.Parent.FullName, "src/results");
+            else
+                save = Path.Combine(current, "src/results");
+
+            Directory.CreateDirectory(save);
+            return save;
         }
     }
 }
