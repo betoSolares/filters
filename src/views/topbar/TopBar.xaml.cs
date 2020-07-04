b1f888246b571a4753babc970897bbeebda527af
Roberto Solares <betosolaresgar@gmail.com>
2020-07-04 13:52:55 -0600

    Add button for change the theme

--- src/views/topbar/TopBar.xaml.cs
+++ src/views/topbar/TopBar.xaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Filters.Views.TopBar
@@ -9,6 +10,14 @@ namespace Filters.Views.TopBar
         public TopBar()
         {
             InitializeComponent();
+        }
+
+        /// <summary>Change the theme of the app</summary>
+        /// <param name="sender">The object that raised the event</param>
+        /// <param name="e">The object that is being handled</param>
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            System.Console.WriteLine("CHANGE");
         }
 
         /// <summary>Initialize all the components</summary>
